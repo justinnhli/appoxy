505459be7934b7508b00ccdec952de14f16d7ff2
Justin Li <justinnhli@gmail.com>
2016-11-06 00:04:24 -0700

    refactor bayes

--- bayes/__init__.py
+++ bayes/__init__.py
@@ -1,14 +1,12 @@
-from os.path import dirname
+from flask import Blueprint, send_from_directory, request
 
-from flask import Blueprint, request
-
-from .bayesnet import BayesNet, HTML
+from .bayesnet import BayesNet
 
 bayes = Blueprint(__name__, __name__, url_prefix=('/'+__name__))
 
 @bayes.route('/')
 def root():
-    return HTML
+    return send_from_directory(__name__, 'bayesnet.html')
 
 @bayes.route('/parse', methods=['POST'])
 def parse():
