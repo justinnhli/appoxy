7fce619e28ca9119add94233e414bd5a3b69b7dc
Justin Li <justinnhli@gmail.com>
2018-03-22 15:35:33 -0700

    [perceptron] convert app to standard format

--- perceptron/__init__.py
+++ perceptron/__init__.py
@@ -1,10 +1,3 @@
 """Graphical explanation of perceptron training."""
 
-from flask import Blueprint, send_from_directory
-
-perceptron = Blueprint(__name__, __name__, url_prefix=('/' + __name__))
-
-
-@perceptron.route('/')
-def root():
-    return send_from_directory(__name__, 'perceptron.html')
+from .app import app as perceptron
