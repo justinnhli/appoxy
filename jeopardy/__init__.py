ac0549c6811ec40482033fcd8b4f1d694502b05f
Justin Li <justinnhli@gmail.com>
2018-03-22 15:54:01 -0700

    [jeopardy] convert app to standard format

--- jeopardy/__init__.py
+++ jeopardy/__init__.py
@@ -1,11 +1,3 @@
 """Jeopardy! board creator and player."""
 
-from os.path import dirname
-
-from flask import Blueprint, send_from_directory
-
-jeopardy = Blueprint(__name__, __name__, url_prefix=('/'+__name__))
-
-@jeopardy.route('/')
-def root():
-    return send_from_directory(__name__, 'jeopardy.html')
+from .app import app as jeopardy
